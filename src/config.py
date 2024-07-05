4687ccd761524f4b0d83310b0b97e24240bed7d8
ITOTI <zhen_yu_@outlook.com>
2024-07-05 11:13:47 +0800

    refactor: Update RoomDataset to handle mask images

--- src/config.py
+++ src/config.py
@@ -5,41 +5,60 @@ class Model_Config:
         pass
 
 COLOR_LABEL = {
-    'f54337': 'Pharmacy',
-    'ea1e63': 'Chinese Pharmacy',
-    '009788': 'Lobby',
+    'f54337': 'Pharmacy', # 药房
+    # 'ea1e63': 'Chinese Pharmacy',
+    '009788': 'Reception desk', # 挂号处
     '673ab7': 'Emergency Department',
-    '3f51b5': 'Infectious Disease Clinic',
-    '2196f3': 'Outpatient Treatment',
-    '03a9f5': 'Radiology',
-    '00bcd5':'Pediatric Unit',
-    'cfd8dd': 'Corridor',
-    '757575': 'Stairs',
-    'bdbdbd': 'Elevator',
-    '9e9e9e': 'Escalator',
-    '4cb050': 'Landscape Space',
-    'ffeb3c': 'Wall',
-    '795547': 'Door',
-    '9c28b1': 'Outside',
-    '8cc34b': 'Endoscopy Center',
-    'cddc39': 'Testing Center',
-    'fec107': 'Gastroenterology',
-    'ff9801': 'Endocrinology',
-    'fe5721': 'Respiratory Medicine',
-    'aaee5b': 'Cardiovascular Medicine',
-    '607d8b': 'Health Examination Department',
-    'efc74e': 'Ophthalmology',
-    'feba57': 'Chinese medicine',
-    'fb8561': 'Oral Medicine',
-    'c6fe83': 'Otorhinolaryngology',
-    '7ca5b9': 'Ultrasound Department',
-    'ad850a': 'Functional Inspection Department',
-    'ffd495': 'Rehabilitation Medicine Department',
-    'fa918b': 'Orthopedics',
-    'ff80ab': 'Nephrology',
-    '21fae7': 'Physical Medicine and Rehabilitation',
-    '8d4eff': 'Hematology',
-    '526cff': 'Dermatology',
-    'e23aff': 'Gynecology',
-    '648b38': 'Obstetrics',
-    'eeff3d': 'Outpatient Surgery'}
+    # '3f51b5': 'Infectious Disease Clinic',
+    '2196f3': 'Outpatient Treatment', # 门诊治疗
+    '03a9f5': 'Radiology', # 放射科
+    '00bcd5':'Pediatric Unit', # 儿科
+    'cfd8dd': 'Corridor', # 走廊
+    '757575': 'Stairs', # 楼梯
+    'bdbdbd': 'Elevator', # 电梯
+    '9e9e9e': 'Escalator', # 扶梯
+    '4cb050': 'Landscape Space', # 景观空间
+    'ffeb3c': 'Wall', # 墙
+    '795547': 'Door', # 门
+    '9c28b1': 'Outside', # 室外
+    '8cc34b': 'Endoscopy Center', # 内镜中心
+    'cddc39': 'Testing Center', # 检验中心
+    'fec107': 'Gastroenterology', # 肠胃科
+    'ff9801': 'Endocrinology', # 内分泌科
+    'fe5721': 'Respiratory Medicine', # 呼吸内科
+    'aaee5b': 'Cardiovascular Medicine', # 心血管内科
+    # '607d8b': 'Health Examination Department',
+    'efc74e': 'Ophthalmology', # 眼科
+    'feba57': 'Chinese medicine', # 中医科
+    'fb8561': 'Oral Medicine', # 口腔科
+    'c6fe83': 'Otorhinolaryngology', # 耳鼻喉科
+    '7ca5b9': 'Ultrasound Department', # 超声科
+    'ad850a': 'Functional Inspection Department', # 功能检查科
+    'ffd495': 'Rehabilitation Medicine Department', # 康复医学科
+    'fa918b': 'Orthopedics', # 骨科
+    'ff80ab': 'Nephrology', # 肾内科
+    '21fae7': 'Physical Medicine and Rehabilitation', # 理疗科
+    '8d4eff': 'Hematology', # 血液科
+    '526cff': 'Dermatology', # 皮肤科
+    'e23aff': 'Gynecology', # 妇科
+    '648b38': 'Obstetrics', # 产科
+    'b373bd': 'Courtyard', # 中庭
+    'e988f9': 'Rheumatology Department', # 风湿免疫科
+    '3f6417': 'Neurology', # 神经内科
+    'b6c709': 'Neurosurgery', # 神经外科
+    'f1dea6': 'Thoracic Surgery', # 胸外科
+    'deade5': 'Colorectal Surgery', # 肛肠外科
+    'a52d24': 'Urology', # 泌尿外科
+    'bb1851': 'General Surgery', # 普外科
+    '075b53': 'Special Outpatient Clinic', # 专家门诊
+    '9685b2': 'Dialysis Center', # 透析中心
+    '727cb1': 'Integrated Traditional Chinese and Western Medicine Department', # 中西医结合科
+    'c37f7a': 'General Outpatient Clinic', # 全科门诊
+    '307a71': 'Reproductive Medicine Department', # 生殖医学科
+    '7028ec': 'Oncology', # 肿瘤科
+    '8e9ef6': 'Gastrointestinal Surgery', # 肝胆外科
+    'f1beb9': 'Family Planning Department', # 计划生育科
+    'bb92a0': 'Occupational Disease Department', # 职业病科
+    '46c3b3': 'Psychology Department', # 心理科
+    'bb97f7': 'Beauty Department', # 美容科
+}
